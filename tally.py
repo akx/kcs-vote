ef444fa816ebc9f64a8e841b46fa101f7c6fddb4
Aarni Koskela <akx@iki.fi>
2017-07-15 17:34:21 +0300

    D'Hondt tally for Desu/C 2017

---
+++ tally.py
@@ -0,0 +1,36 @@
+import argparse
+import fractions
+from collections import defaultdict, Counter
+from tabulate import tabulate
+
+ap = argparse.ArgumentParser()
+ap.add_argument('file')
+args = ap.parse_args()
+
+votes = defaultdict(list)
+
+with open(args.file) as infp:
+	for line in infp:
+		line = line.strip()
+		if not line:
+			continue
+		name, num = line.split(None, 1)
+		assert num <= 5
+		num = fractions.Fraction(1, int(num))
+		votes[name.title()].append(num)
+
+table = [
+	(
+		name,
+		float(sum(name_votes)),
+		str(sum(name_votes)),
+		', '.join([
+			'%s: %d' % (vote, count)
+			for (vote, count)
+			in Counter('%d/%d' % (v.numerator, v.denominator) for v in name_votes).most_common()
+		])
+	)
+	for name, name_votes
+	in sorted(votes.items(), key=lambda p: sum(p[1]), reverse=True)
+]
+print(tabulate(table))
