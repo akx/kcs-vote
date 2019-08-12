9dfadc0ba00c7bc26f0ad87a073f87e9599d2c9f
Aarni Koskela <akx@iki.fi>
2019-08-12 11:33:15 +0300

    Run black

--- tally.py
+++ tally.py
@@ -4,34 +4,36 @@ from collections import defaultdict, Counter
 from tabulate import tabulate
 
 ap = argparse.ArgumentParser()
-ap.add_argument('file')
+ap.add_argument("file")
 args = ap.parse_args()
 
 votes = defaultdict(list)
 
 with open(args.file) as infp:
-	for line in infp:
-		line = line.strip()
-		if not line:
-			continue
-		name, num = line.split(None, 1)
-		num = int(num)
-		assert num <= 5
-		frac = fractions.Fraction(1, num)
-		votes[name.title()].append(frac)
+    for line in infp:
+        line = line.strip()
+        if not line:
+            continue
+        name, num = line.split(None, 1)
+        num = int(num)
+        assert num <= 5
+        frac = fractions.Fraction(1, num)
+        votes[name.title()].append(frac)
 
 table = [
-	(
-		name,
-		float(sum(name_votes)),
-		str(sum(name_votes)),
-		', '.join([
-			'%s: %d' % (vote, count)
-			for (vote, count)
-			in Counter('%d/%d' % (v.numerator, v.denominator) for v in name_votes).most_common()
-		])
-	)
-	for name, name_votes
-	in sorted(votes.items(), key=lambda p: sum(p[1]), reverse=True)
+    (
+        name,
+        float(sum(name_votes)),
+        str(sum(name_votes)),
+        ", ".join(
+            [
+                "%s: %d" % (vote, count)
+                for (vote, count) in Counter(
+                    "%d/%d" % (v.numerator, v.denominator) for v in name_votes
+                ).most_common()
+            ]
+        ),
+    )
+    for name, name_votes in sorted(votes.items(), key=lambda p: sum(p[1]), reverse=True)
 ]
 print(tabulate(table))
